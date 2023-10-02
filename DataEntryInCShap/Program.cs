4300531be8a383b973956d2098993e225c36d345
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-10-01 21:57:23 -0400

    Adicionar arquivos de projeto.

---
+++ DataEntryInCShap/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DataEntryInCShap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto:");
+            double price = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o seu ultimo nome, idade e altuta (na mesma linha)");
+            string[] vet = Console.ReadLine().Split(' ');
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(price);
+            Console.WriteLine(vet[0]);
+            Console.WriteLine(vet[1]);
+            Console.WriteLine(vet[2]);
+        }
+    }
+}
